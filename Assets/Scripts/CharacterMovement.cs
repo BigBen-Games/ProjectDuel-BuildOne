737c0bd58c98cab75ab66db7571bdb8ea27422f0
Ben Lillard <bennettlillard@gmail.com>
2025-02-08 17:38:42 -0600

    CharacterMovement Script implemented

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,20 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speedValue;
-    public float jumpForce;
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;    // Speed of movement
+    public float jumpForce = 5f;    // Force applied when jumping
+    public int maxJumpCount; //how many times a character can jump
+    float jumpCount = 0; //the current number of jumps
+    private Rigidbody2D rb;         // Rigidbody2D component
+
+    private bool isGrounded;        // Check if the player is on the ground
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Equations for this build by Microsoft Copilot
         
+        // Move left and right
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.W) && (isGrounded | jumpCount < maxJumpCount))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, (jumpForce + jumpCount));
+            jumpCount++;
+            isGrounded = false; // Player is no longer on the ground
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player is on the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
     }
 }
